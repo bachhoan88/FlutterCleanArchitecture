d149e85602de3be96a82a5c712a257dad2fa48be
Bach Ngoc Hoai <bach.ngoc.hoai@sun-asterisk.com>
2021-03-11 16:24:44 +0700

    Try CI Coverage

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,11 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_macos
 import path_provider_macos
 import shared_preferences_macos
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
